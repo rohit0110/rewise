173195a8dd56495a7b9bf9f2bb41edf14356c0bf
rohit0110 <rohit7rathore15@gmail.com>
2025-02-05 14:26:32 +0530

    Setup app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  StatefulElement createElement() {
+    // TODO: implement createElement
+    return super.createElement();
+  }
+}
