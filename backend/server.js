09a0d90cc992bc7eef7736145380fd9a1e4048ab
rohit0110 <rohit7rathore15@gmail.com>
2025-04-02 02:12:11 +0530

    Setup Mongodb and backend connection, setup frontend structure

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+//connect to mongo
+mongoose.connect("mongodb://admin:adminpassword@localhost:27017/admin", { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  })
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.error("MongoDB connection error:", err));
+// Define routes and middleware
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+const todoSchema = new mongoose.Schema({
+    task: String,
+    completed: Boolean,
+  });
+
+const Todo = mongoose.model('Todo', todoSchema);
